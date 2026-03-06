7e6754f92c4dc238be6966286ea10edd82c7115a
vasi <vasi.che@gmail.com>
2026-03-06 14:52:44 +0500

    align N

--- src/main/java/net/javaguides/springbootsecurity/web/HomeController.java
+++ src/main/java/net/javaguides/springbootsecurity/web/HomeController.java
@@ -5,7 +5,9 @@ import net.javaguides.springbootsecurity.repositories.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.logging.Logger;
@@ -26,10 +28,28 @@ public class HomeController {
         return "userhome";
     }
 
+    @GetMapping("/messages/delete/{n}")
+    public String deleteMessage(@PathVariable Integer n) {
+        logger.info("Delete message: n=" + n);
+        messageRepository.deleteById(n);
+        return "redirect:/home";
+    }
+
+    @GetMapping("/messages/edit/{n}")
+    public String editMessage(@PathVariable Integer n, Model model) {
+        logger.info("Edit message: n=" + n);
+        Message message = messageRepository.getById(n);
+        logger.info("Received message:" + message);
+        model.addAttribute("content", message.getContent());
+        model.addAttribute("n", message.getN());
+        return "redirect:/home";
+    }
+
     @PostMapping("/messages")
     public String saveMessage(Message message) {
         logger.info("Save message: " + message.toString());
         messageRepository.save(message);
         return "redirect:/home";
     }
+
 }
